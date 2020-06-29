3de9e8332270ee9a0a56157607d64d1c171c1d01
danieldeyisus <danieldejesusgs@gmail.com>
2020-06-29 19:26:36 -0400

    hasher

---
+++ src/Recursos/Hasher.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+/**
+ *
+ * @author Jota Wolf
+ */
+import java.nio.charset.Charset;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+
+public class Hasher {
+    public final Integer DEFAULT_ITERATIONS = 10000;
+    public final String algorithm = "pbkdf2_sha256";
+
+    public Hasher() {}
+
+    public String getEncodedHash(String password, String salt, int iterations) {
+        // Returns only the last part of whole encoded password
+        SecretKeyFactory keyFactory = null;
+        try {
+            keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Could NOT retrieve PBKDF2WithHmacSHA256 algorithm");
+            System.exit(1);
+        }
+        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt.getBytes(Charset.forName("UTF-8")), iterations, 256);
+        SecretKey secret = null;
+        try {
+            secret = keyFactory.generateSecret(keySpec);
+        } catch (InvalidKeySpecException e) {
+            System.out.println("Could NOT generate secret key");
+            e.printStackTrace();
+        }
+
+        byte[] rawHash = secret.getEncoded();
+        byte[] hashBase64 = Base64.getEncoder().encode(rawHash);
+
+        return new String(hashBase64);
+    }
+
+    public String encode(String password, String salt, int iterations) {
+        // returns hashed password, along with algorithm, number of iterations and salt
+        String hash = getEncodedHash(password, salt, iterations);
+        return String.format("%s$%d$%s$%s", algorithm, iterations, salt, hash);
+    }
+
+    public String encode(String password, String salt) {
+        return this.encode(password, salt, this.DEFAULT_ITERATIONS);
+    }
+
+    public boolean checkPassword(String password, String hashedPassword) {
+        // hashedPassword consist of: ALGORITHM, ITERATIONS_NUMBER, SALT and
+        // HASH; parts are joined with dollar character ("$")
+        String[] parts = hashedPassword.split("\\$");
+        if (parts.length != 4) {
+            // wrong hash format
+            return false;
+        }
+        Integer iterations = Integer.parseInt(parts[1]);
+        String salt = parts[2];
+        String hash = encode(password, salt, iterations);
+
+        return hash.equals(hashedPassword);
+    }
+
+   
+    public static String passwordShouldMatch(String password, String expectedHash) {
+        Hasher hasher = new Hasher();
+
+        if (hasher.checkPassword(password, expectedHash)) {
+            System.out.println(" => OK");
+            return expectedHash;
+        } else {
+            return "error";
+        }
+    }
+}
